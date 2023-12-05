d8841ec6f296754b0598c6821166445aa93b6c49
Prokhor Ozornin <prokhor.ozornin@yandex.ru>
2023-12-05 16:19:58 +0500

    IBuilder interface

---
+++ Catharsis.Commons/Interfaces/IBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Catharsis.Commons;
+
+/// <summary>
+///   <para></para>
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IBuilder<out T>
+{
+  /// <summary>
+  ///   <para></para>
+  /// </summary>
+  /// <returns></returns>
+  T Build();
+}
